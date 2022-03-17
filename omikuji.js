825b9f5b7dd614a5da42bdce9c3d769aee509035
Marina04mocha13 <mari_td14@keio.jp>
2022-03-17 16:42:37 +0900

    [Finish]Webアプリケーションを作ろう-2

--- omikuji.js
+++ omikuji.js
@@ -1,17 +1,13 @@
 let username;
 let userresult;
 username = prompt("お名前を教えて下さい。");
-if(username == ""){
-    username = "名無し"; 
- }
+if(username = ""){
+    username = "名無し";
+}
 document.getElementById("name").innerHTML = username;
 
-
 let rand = Math.floor( Math.random() * 5) ;
-if (rand == 0) {
-    userresult = "大吉";
-  }
-  if (rand == 1) {
+if (rand == 1) {
     userresult = "中吉";
   }
   if (rand == 2) {
